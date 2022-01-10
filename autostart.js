800f65dc0e7fd3ee98df558811d48f6be1333e91
whiskeyfur <john.walker.ii@gmail.com>
2022-01-10 12:45:42 -0800

    Updated autostart to not call itself

--- autostart.js
+++ autostart.js
@@ -2,5 +2,5 @@ import * as env from "lib-env.js"
 
 /** @param {import(".").NS} ns **/
 export async function main(ns) {
-    ns.exec("autostart.js", ns.getHostname())
+    ns.exec("crontab.js", ns.getHostname())
 }
