dd807ad564143dc467babc9ebcb7b39daeb1c3d5
whiskeyfur <john.walker.ii@gmail.com>
2022-01-11 00:06:42 -0800

    cmd-targets - Will assess current viable targets for best return on investment (time)

---
+++ cmd-targets.js
@@ -0,0 +1,19 @@
+import * as Svrs from "./lib-servers.js"
+/** @param {import(".").NS} ns **/
+export async function main(ns) {
+  var servers = await Svrs.getServers(ns);
+  servers.sort((a,b) => incomePerSecond(ns,a) - incomePerSecond(ns,b))
+  servers
+  .filter(s => ns.getServerMoneyAvailable(s))
+  .filter(s => ns.getServerMaxMoney(s))
+  .filter(s => incomePerSecond(ns, s))
+  .forEach(s => 
+    ns.tprint(
+      s.padStart(18) + " -- " + ns.nFormat(incomePerSecond(ns, s) * 60, "0.00a") + "/min"
+    )
+  )
+}
+
+function incomePerSecond(ns, s) {
+  return ns.getServerMoneyAvailable(s) * ns.hackAnalyze(s) / ns.getHackTime(s);
+}
