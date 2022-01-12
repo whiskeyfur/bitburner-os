b8987f301dfa9b50a211c0351bd88293f0766646
whiskeyfur <john.walker.ii@gmail.com>
2022-01-12 03:10:28 -0800

    Altered the algorithm for farming

--- cron-farm.js
+++ cron-farm.js
@@ -5,10 +5,11 @@ export async function main(ns) {
     //ns.tail();
     ns.clearLog();
     ns.disableLog("ALL");
+    var threadLimit = 999999;
+
 
     var workers = (await Servers.getServers(ns))
     .filter(s => ns.hasRootAccess(s))
-    .filter(s => s != "home")
     .sort((a,b) => 
         (ns.getServerMaxRam(b) - ns.getServerUsedRam(b)) -
         (ns.getServerMaxRam(a) - ns.getServerUsedRam(a))
@@ -17,66 +18,40 @@ export async function main(ns) {
 
     // weaken
     var targets = (await Servers.getServers(ns))
-    .filter(s => ns.hasRootAccess(s))
     .filter(s => ns.getServerMaxMoney(s))
     .filter(s => ns.getServerRequiredHackingLevel(s) <= ns.getHackingLevel())
-    .filter(s => ns.getServerSecurityLevel(s) > ns.getServerMinSecurityLevel(s))
+    /*
     .sort((a,b) => 
         (ns.getServerSecurityLevel(a) - ns.getServerMinSecurityLevel(a)) -
         (ns.getServerSecurityLevel(b) - ns.getServerMinSecurityLevel(b)) 
-    );
+    )
+    */
+    .sort((a,b) => ns.getServerRequiredHackingLevel(a) - ns.getServerRequiredHackingLevel(b))
+    ;
 
     for (var t of targets) {
-        var threadsNeeded = Math.min(100,(ns.getServerSecurityLevel(t) - ns.getServerMinSecurityLevel(t)) / ns.weakenAnalyze(1));
+        var hackThreadsNeeded   = ns.hackAnalyzeThreads(t, ns.getServerMoneyAvailable(t) * 0.01)
+        var growThreadsNeeded   = ns.growthAnalyze(t, ns.getServerMaxMoney(t) / ns.getServerMoneyAvailable(t) + 0.1)
+        var weakenThreadsNeeded = (
+            ns.growthAnalyzeSecurity(growThreadsNeeded)  * ns.getWeakenTime(t) / ns.getGrowTime(t)
+            + ns.hackAnalyzeSecurity(hackThreadsNeeded)  * ns.getWeakenTime(t) / ns.getHackTime(t)
+            + ns.getServerSecurityLevel(t)
+            - ns.getServerMinSecurityLevel(t)
+        ) / ns.weakenAnalyze(1)
+
         for (var w of workers) {
             for (var ps of ns.ps(w)) {
-                if (ps.filename == "cmd-weaken.js" && ps.args[0] == t) {
-                    threadsNeeded -= ps.threads;
-                }
+                if (ps.filename == "cmd-weaken.js" && ps.args[0] == t) weakenThreadsNeeded -= ps.threads;
+                if (ps.filename == "cmd-grow.js"   && ps.args[0] == t)   growThreadsNeeded -= ps.threads;
+                if (ps.filename == "cmd-hack.js"   && ps.args[0] == t)   hackThreadsNeeded -= ps.threads;
             }
         }
-        distribute(ns, workers, "cmd-weaken.js", threadsNeeded, t)
-    }
 
-    // Growth
-    targets = (await Servers.getServers(ns))
-    .filter(s => ns.hasRootAccess(s))
-    .filter(s => ns.getServerMaxMoney(s))
-    .filter(s => ns.getServerRequiredHackingLevel(s) <= ns.getHackingLevel())
-    .filter(s => ns.getServerMoneyAvailable(s) < ns.getServerMaxMoney(s));
+        distribute(ns, workers, "cmd-weaken.js", weakenThreadsNeeded, t)
+        distribute(ns, workers, "cmd-hack.js"  , hackThreadsNeeded, t)
+        distribute(ns, workers, "cmd-grow.js"  , growThreadsNeeded, t)
+    }
     
-
-    for (var t of targets) {
-        var threadsNeeded = Math.min(100,ns.growthAnalyze(t, ns.getServerMaxMoney(t) / ns.getServerMoneyAvailable(t)))
-        for (var w of workers) {
-            for (var ps of ns.ps(w)) {
-                if (ps.filename == "cmd-grow.js" && ps.args[0] == t) 
-                    threadsNeeded -= ps.threads
-            }
-        }
-        distribute(ns, workers, "cmd-grow.js", threadsNeeded, t)
-    }
-
-    // Hack phase
-    targets = (await Servers.getServers(ns))
-    .filter(s => ns.hasRootAccess(s))
-    .filter(s => ns.getServerMaxMoney(s))
-    .filter(s => ns.getServerRequiredHackingLevel(s) <= ns.getHackingLevel())
-    .filter(s => ns.getServerMoneyAvailable(s) > ns.getServerMaxMoney(s) * 0.95)
-    .sort((a,b) => (ns.getServerMoneyAvailable(b) * ns.hackAnalyze(b) * ns.hackAnalyzeChance(b))-(ns.getServerMoneyAvailable(a) * ns.hackAnalyze(a) * ns.hackAnalyzeChance(a)));
-
-    for (var t of targets) {
-        var threadsNeeded = Math.min(100,Math.max(ns.hackAnalyzeThreads(t, ns.getServerMoneyAvailable(t) * 0.05) / ns.hackAnalyzeChance(t), 1))
-        var memAvail = ns.getServerMaxRam(w) - (w == "home" ? 128 : 0) - ns.getServerUsedRam(w);
-        for (var w of workers) {
-            for (var ps of ns.ps(w)) {
-                if (ps.filename == "cmd-hack.js" && ps.args[0] == t) 
-                    threadsNeeded -= ps.threads
-            }
-        }
-        distribute(ns, workers, "cmd-hack.js", threadsNeeded, t)
-    }
-
 }
 
 /** @param {import(".").NS} ns **/
@@ -86,8 +61,15 @@ function incomePerSec(ns, s) {
 
 /** @param {import(".").NS} ns **/
 function distribute(ns, workers, script, threadsNeeded, target) {
-    for (var w of workers) {
-        var memAvail = ns.getServerMaxRam(w) - (w == "home" ? 64 : 0) - ns.getServerUsedRam(w);
+    for (var w of workers
+            .sort(
+                (a,b) => (
+                    (ns.getServerMaxRam(b) - ns.getServerUsedRam(b))
+                    - (ns.getServerMaxRam(a) - ns.getServerUsedRam(a))
+                )
+            )
+    ) {
+        var memAvail = ns.getServerMaxRam(w) - (w == "home" ? 256 : 0) - ns.getServerUsedRam(w);
         var threads = Math.max(Math.min(Math.floor(memAvail / ns.getScriptRam(script)), threadsNeeded),1);
         if ((threadsNeeded > 0) && (memAvail > ns.getScriptRam(script))) {
             if (ns.exec(script, w, threads, target, ns.getTimeSinceLastAug())) {
