b0c5a9cac61ec1414638d348aa0423a244b8873d
whiskeyfur <john.walker.ii@gmail.com>
2022-01-14 00:50:46 -0800

    Bad update, corrected with latest version

--- cmd-env.js
+++ cmd-env.js
@@ -3,22 +3,17 @@ import {data} from "./sys-database"
 /** @param {import("..").NS} ns **/
 export async function main(ns) {
     if (!ns.args.length) {
-        ns.tprint("Fetching all...")
         Object.keys(data).forEach(k => {
             ns.tprint(k.padEnd(30, "_") + " " + data[k]);
         });
-    } else {
+    } else { 
         if (ns.args[0] == "set") {
-            if (Object.keys(data).includes(ns.args[1])) data[ns.args[1]] = ns.args[2]
-            else ns.tprint("Not a valid setting")
+            if (data[ns.args[1]] !== null)
+                data[ns.args[1]] = ns.args[2]
+            else
+                ns.tprint("Not a valid setting")
         } else if (ns.args[0] == "add") {
             data[ns.args[1]] = ns.args[2]
-        } else if (ns.args[0] == "unset") {
-            delete(data[ns.args[1]])
         }
     }
-}
-
-export async function autocomplete(d, args) {
-    return Object.keys(data);
 }
